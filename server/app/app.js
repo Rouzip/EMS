75f907a312bd86e8041c5a9f8d7552a31d7ca55b
Rouzip <1226015390@qq.com>
2018-04-02 19:36:48 +0800

    chang allowed head

--- server/app/app.js
+++ server/app/app.js
@@ -16,7 +16,7 @@ const app = new Koa()
 // 解决跨域，添加头
 app.use(async(ctx, next) => {
   ctx.set('Access-Control-Allow-Credentials', 'true')
-  ctx.set('Access-Control-Allow-Origin', 'http://localhost:8080')
+  // ctx.set('Access-Control-Allow-Origin', 'http://localhost:8080')
   ctx.set('Access-Control-Allow-Origin', 'http://0.0.0.0:8080')
   await next()
 })
